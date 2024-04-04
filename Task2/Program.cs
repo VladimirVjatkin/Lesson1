27ee98e33a52899092c1c5024d186f808a1ed4f2
Vladimir Vjatkin <viatkin@volny.cz>
2024-04-04 18:10:00 +0200

    we add various int long double + and *

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int i1=5;
+int i2=6;
+int sum=i1+i2;
+Console.WriteLine("Sum=");
+Console.WriteLine(sum);
+Console.WriteLine(i1-i2);
+
+long l1=6;
+long sum2=l1+i1;
+Console.WriteLine(sum2);
+
+Double d1=5.5;
+double sum3=d1+i1;
+Console.WriteLine(sum3);
+
+int product1=i1*i2;
+long product2=l1*i1;
+double product3=d1*i1;
+
+Console.WriteLine("product");
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
