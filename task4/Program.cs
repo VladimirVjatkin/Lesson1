c284e0c10e95995898ed6583454258051b3740d4
Vladimir Vjatkin <viatkin@volny.cz>
2024-04-04 18:59:46 +0200

    L1.T4 poisk naibolsego cisla

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 6;
+int b = 8;
+int c = 9;
+int d = 50;
+int e = 40;
+
+int max = a;
+
+if (b > max)
+{ max = b;}
+if (c > max)
+{ max = c;}
+if (d > max)
+{ max = d;}
+if (e > max)
+{ max = e;}
+
+Console.WriteLine(max);
