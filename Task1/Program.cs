4690371ec0f91d26b2284b9251a1d880d3f1ffea
Vladimir Vjatkin <viatkin@volny.cz>
2024-04-04 17:42:57 +0200

    we add few veriables - lesson1/task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("PRIVET porosjata!");
+﻿
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("PRIVET porosjata!");
 //info
+
+int value = 100;
+Console.WriteLine(value/2);
+
+double d=5.5;
+char c='!';
+bool b=true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
